3396c29acee9cd97bde2723e109a878b02fff235
Maria Florencia Gallardo <mflorenciagallardo@gmail.com>
2024-06-29 15:08:32 -0300

    Detalles agregados

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
   res.send("Hola Express!");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
