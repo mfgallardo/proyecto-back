81ad75bfff2e890418292d35e770b3a68e2e4760
Maria Florencia Gallardo <mflorenciagallardo@gmail.com>
2024-07-05 23:10:45 -0300

    Agregar imagen

--- controllers/pacientes.controller.js
+++ controllers/pacientes.controller.js
@@ -2,6 +2,9 @@
 
 const db = require("../db/db");
 
+const fs = require("fs");
+const path = require("path");
+
 // Traer todos los resultados
 
 const index = (req, res) => {
@@ -37,6 +40,9 @@ const show = (req, res) => {
 // Crear un paciente
 
 const store = (req, res) => {
+  console.log(req.file);
+
+  const { filename } = req.file;
   const {
     name,
     surname,
@@ -48,14 +54,24 @@ const store = (req, res) => {
   } = req.body;
 
   const sql =
-    "INSERT INTO patients (name, surname, email, phone_number, date_of_birth, address, national_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    "INSERT INTO patients (name, surname, email, phone_number, date_of_birth, address, national_id, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
   db.query(
     sql,
-    [name, surname, email, phone_number, date_of_birth, address, national_id],
+    [
+      name,
+      surname,
+      email,
+      phone_number,
+      date_of_birth,
+      address,
+      national_id,
+      filename,
+    ],
     (error, result) => {
       console.log(result);
       if (error) {
         console.error("Error al buscar paciente:", error);
+        fs.unlinkSync(path.join(__dirname, "./uploads", filename));
         return res.status(500).json({ error: "Error interno del servidor" });
       }
       const paciente = { ...req.body, id: result.insertId };
@@ -67,6 +83,11 @@ const store = (req, res) => {
 // Modificar datos de un paciente
 
 const update = (req, res) => {
+  console.log(req.file);
+
+  let sql =
+    "UPDATE patients SET name = ?, surname = ?, email = ?, phone_number = ?, date_of_birth = ?, address = ?, national_id = ? WHERE patient_id = ?";
+
   const { id } = req.params;
   const {
     name,
@@ -78,34 +99,42 @@ const update = (req, res) => {
     national_id,
   } = req.body;
 
-  const sql =
-    "UPDATE patients SET name = ?, surname = ?, email = ?, phone_number = ?, date_of_birth = ?, address = ?, national_id = ? WHERE patient_id = ?";
-  db.query(
-    sql,
-    [
-      name,
-      surname,
-      email,
-      phone_number,
-      date_of_birth,
-      address,
-      national_id,
-      id,
-    ],
-    (error, result) => {
-      console.log(result);
-      if (error) {
-        console.error("Error al buscar paciente:", error);
-        return res.status(500).json({ error: "Error interno del servidor" });
-      }
+  const values = [
+    name,
+    surname,
+    email,
+    phone_number,
+    date_of_birth,
+    address,
+    national_id,
+  ];
 
-      if (result.affectedRows === 0) {
-        return res.status(404).json({ message: "No existe el paciente" });
-      }
-      const paciente = { ...req.body, ...req.params };
-      res.json(paciente);
+  if (req.file) {
+    const { filename } = req.file;
+    sql =
+      "UPDATE patients SET name = ?, surname = ?, email = ?, phone_number = ?, date_of_birth = ?, address = ?, national_id = ?, image = ? WHERE patient_id = ?";
+    values.push(filename);
+  }
+
+  values.push(id);
+
+  db.query(sql, values, (error, result) => {
+    // console.log(result);
+    if (error) {
+      console.error("Error al buscar paciente:", error);
+      return res.status(500).json({ error: "Error interno del servidor" });
     }
-  );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "No existe el paciente" });
+    }
+
+    if (result.affectedRows === 1) {
+      // fs.unlink
+    }
+    const paciente = { ...req.body, ...req.params };
+    res.json(paciente);
+  });
 };
 
 // Borrar un paciente
--- routes/pacientes.router.js
+++ routes/pacientes.router.js
@@ -1,5 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+
+    // Verifica tipo de archivo
+    const mimetype = fileTypes.test(file.mimetype);
+
+    // Verifica la extensión del archivo
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    cb("No es un archivo permitido");
+  },
+
+  // Define tamaño máximo de archivo
+  limits: { fileSize: 1024 * 1024 * 1 },
+});
 
 const controller = require("../controllers/pacientes.controller");
 
@@ -10,7 +47,7 @@ router.get("/", controller.index);
 router.get("/:id", controller.show);
 
 // POST
-router.post("/", controller.store);
+router.post("/", upload.single("image"), controller.store);
 
 // PUT /pacientes/:id
 router.put("/:id", controller.update);
